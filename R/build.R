91d644efe1474d76d0396e5a29ee63d40b6bd7b1
jacintak <48912454+jacintak@users.noreply.github.com>
2020-12-16 18:58:39 +0000

    fixed yaml problem in header. Updated intro blogs, added slugs,images

--- R/build.R
+++ R/build.R
@@ -1,11 +1,11 @@
 # Make CV PDF
-# rmarkdown::render('content/cv/index.Rmd', output_format = rmarkdown::pdf_document(keep_tex = FALSE), output_dir = "static/files/", output_file = "Kong_JD_CV.pdf")
+rmarkdown::render('content/cv/index.Rmd', output_format = rmarkdown::pdf_document(keep_tex = FALSE), output_dir = "static/files/", output_file = "Kong_JD_CV.pdf")
 
 # make gitbook
 # blogdown::build_dir("static") doesn't work because we want a gitbook that knits with render_book
 # whereas build_dir uses rmakrdown::render() thus giving html files
 old <- getwd()
 setwd("static/teaching/GLM/")
-# bookdown::clean_book(clean =FALSE)
-# bookdown::render_book(input = "index.Rmd")
+bookdown::clean_book(clean = TRUE)
+bookdown::render_book(input = "index.Rmd")
 setwd(old)
